3dfc4a5f5d2fe6fac5a81cf3d00bd27c17f5e664
Tabrez alam <89697135+tabrezzeta@users.noreply.github.com>
2024-01-12 13:00:30 +0530

    Update modal app.js

--- modal app.js
+++ modal app.js
@@ -23,7 +23,6 @@ function outside(e){
         modal_div.classList.remove("open_modal_app");
     }
 }
-
 /////////// QR Code Generator
  var qr_img = document.getElementById("qr_img");
  var url_input = document.getElementById("input_url");
@@ -32,9 +31,11 @@ function outside(e){
 
  var api = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=`;
 
+ var img_src ;
+ var user_input;
  function generate_qr(){
 
-    var user_input = url_input.value;
+    user_input = url_input.value;
     
     var validate = user_input.includes(".in") || user_input.includes(".com") || user_input.includes(".gov");
 
@@ -57,24 +58,43 @@ function outside(e){
 
             var downloadBtn = document.querySelector(".download_qr");
             downloadBtn.style.display = "block";
-
         }
-        
         qr_img.src = api+url_input.value;
+        img_src =  qr_img.src;
     }
-    
  }
 
- //////////
 
- function download_qr(){
-    var qr = document.createElement("a");
-    qr.href = "https://www.learningcontainer.com/wp-content/uploads/2019/09/sample-pdf-file.pdf";
-    qr.setAttribute("target", "_blank");
-    // qr.download = "test.pdf";
-    document.body.appendChild(qr);
-    qr.click();
-    // qr.remove();
-    
-    console.log(qr);
- }
+ ////////////
+ function download_qr() {
+    // Get the image URL
+    var imageUrl =  img_src;
+  
+    // Fetch the image data
+    fetch(imageUrl)
+      .then(response => response.blob())
+      .then(blob => {
+        // Create a link element
+        var link = document.createElement('a');
+
+        // Create a URL for the Blob
+        var url = window.URL.createObjectURL(blob);
+  
+        // Set the link's href attribute to the Blob URL
+        link.href = url;
+  
+        // Set the download attribute with the desired filename
+        link.download = `QR-${user_input}-image.jpg`;
+  
+        // Append the link to the document
+        document.body.appendChild(link);
+  
+        // Trigger a click event on the link
+        link.click();
+  
+        // Remove the link and revoke the Blob URL
+        document.body.removeChild(link);
+        window.URL.revokeObjectURL(url);
+      })
+      .catch(error => console.error('Error fetching image:', error));
+  }
